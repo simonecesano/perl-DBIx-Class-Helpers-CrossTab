d4b5687b1f40e7ff020363056e00ec33aad98420
Simone Cesano <simonecesano@libero.it>
2016-12-11 22:22:44 +0100

    working version

--- lib/DBIx/Class/ResultClass/ArrayRefInflator.pm
+++ lib/DBIx/Class/ResultClass/ArrayRefInflator.pm
@@ -2,12 +2,11 @@ package DBIx::Class::ResultClass::ArrayRefInflator;
 use Data::Dump qw/dump/;
 
 sub inflate_result {
-    # dump \@_;
-    # dump $_[3];
-    $_[2];
-    # return $self;
-    # return [ map { ref $_ ? ref $_ : $_ } @_ ];
-    return $_[2]
+    return \@{$_[2]}{$_[0]->_resolved_attrs->{as}}
+}
+
+sub header {
+    return shift->_resolved_attrs->{as}
 }
 
 1;
